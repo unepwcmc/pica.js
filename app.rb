60391733c935c1f315635d4c86194bde1874dba2
James Cox <th3james@gmail.com>
2012-11-09 17:09:14 -0500

    Undo stupidity in app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
-# Primary domain name of your application. Used in the Apache configs
-set :domain, "unepwcmc-005.vm.brightbox.net"
+require 'sinatra/assetpack'
+require 'json'
 
-## List of servers
-server "unepwcmc-005.vm.brightbox.net", :app, :web, :db, :primary => true
+class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',     from: 'app/js'
+    serve '/lib',    from: 'app/lib'       # Optional
+    serve '/images', from: 'app/images'    # Optional
+  }
+
+  get '/' do
+    File.read(File.join('app', 'index.html'))
+  end
+
+  post '/polygon' do
+    # TODO Actually store the polygon somewhere
+    @json = JSON.parse(request.body.read)
+    @json['id'] = 5
+    @json['analysis_id'] = 1
+
+    @json.to_json
+  end
+
+  get '/polygon/:id/calculated_layer_stats' do
+    # TODO Return calculated stats for the given polygon
+    [
+      {name: 'Carbon', value: 5},
+      {name: 'Forest', value: 8}
+    ].to_json
+  end
+end

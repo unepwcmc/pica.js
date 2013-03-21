00c768cc1296764b78dd4e7f5c09fa3b865f9858
James Cox <th3james@gmail.com>
2013-03-21 11:18:02 +0000

    Test for failing IE behavior

--- test/spec/cors.js
+++ test/spec/cors.js
@@ -1,7 +1,40 @@
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      expect(true).to.equal(true);
+describe('Pica.Models.Workspace', function(){
+  describe('when saving to a working magpie server', function(){
+    before(function(done){
+      // TODO put in mapExistsHelper
+      var map = L.map('map',{
+        center: [24.5,54],
+        zoom: 9
+      });
+
+      // TODO application exists
+      var pica = new Pica.Application({
+        magpieUrl: "http://magpie.unepwcmc-005.vm.brightbox.net",
+        projectId: 5,
+        map: map
+      });
+      // Create a new workspace to work in
+      pica.newWorkspace();
+
+      success = sinon.spy(function(){
+        done();
+      });
+      error = sinon.spy(function(){
+        done();
+      });
+
+      // create a file view
+      fileView = pica.currentWorkspace.save({
+        success: success,
+        error: error
+      });
+    });
+
+    it('calls the success callback', function(){
+      expect(success.calledOnce).to.equal(true);
+    });
+    it('does not call the error callback', function(){
+      expect(error.calledOnce).to.equal(false);
     });
   });
 });
